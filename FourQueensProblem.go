68ae179657b6bf627c0a98f98b27238a4966c1d4
SidSin <siddharthsch@gmail.com>
2021-04-04 06:11:24 +0530

    Update FourQueensProblem.go
    This correctly places the three queens

--- FourQueensProblem.go
+++ FourQueensProblem.go
@@ -33,33 +33,102 @@ func printboard(B [4][4]int) {
 	printrow(B, 3)
 }
 
-func allowed(x1 int, y1 int, x2 int, y2 int) bool {
+func allowed(board [4][4]int, newqueenrowpos int, newqueencolpos int) bool {
 
 	answer := true
+	answer = checkrowallowed(board, newqueenrowpos, answer)
 
-	if x1 == x2 {
-		answer = false
+	if answer {
+		answer = checkcolumnallowed(board, newqueencolpos, answer)
 	}
 
-	if y1 == y2 {
-		answer = false
+	if answer {
+		answer = checkcdiagonalallowed(board, newqueenrowpos, newqueencolpos, answer)
+	}
+	return answer
+}
+
+func checkcdiagonalallowed(board [4][4]int, newqueenrpos int, newqueencpos int, answer bool) bool {
+	qcount := 0
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board); j++ {
+
+			if abs(newqueenrpos, i) == abs(newqueencpos, j) {
+				if board[i][j] == 1 {
+					qcount++
+				}
+			}
+
+		}
 	}
 
-	if abs(x2, x1) == 1 && abs(y2, y1) == 1 {
+	if qcount > 1 {
 		answer = false
 	}
 
 	return answer
 }
 
-func fn0(board [4][4]int, r2 int, r int) {
-	board[r2][1] = 1
-	if allowed(r, 0, r2, 1) {
-		printboard(board)
-		fmt.Println(" ")
-	} else {
-		board[r2][1] = 0
+func checkcolumnallowed(board [4][4]int, newqueenxpos int, answer bool) bool {
+	qcount := 0
+
+	for i := 0; i < len(board); i++ {
+		if board[i][newqueenxpos] == 1 {
+			qcount++
+		}
 	}
+	if qcount > 1 {
+		answer = false
+	}
+	return answer
+}
+
+func checkrowallowed(board [4][4]int, newqueenrowpos int, answer bool) bool {
+	qcount := 0
+
+	//check all coulmns for new queen row position
+	for i := 0; i < len(board); i++ {
+		if board[newqueenrowpos][i] == 1 {
+			qcount++
+		}
+	}
+	if qcount > 1 {
+		answer = false
+	}
+	return answer
+}
+
+func placethirdqueen(b [4][4]int) {
+
+	for r3 := 0; r3 < 4; r3++ {
+		//place queen on column 3
+		b[r3][2] = 1
+
+		if allowed(b, r3, 2) {
+			printboard(b)
+			fmt.Println(" ")
+		} else {
+			b[r3][2] = 0
+		}
+	}
+
+}
+
+func placesecondqueen(board [4][4]int) {
+
+	for r := 0; r < len(board); r++ {
+		//place q on column 2
+		board[r][1] = 1
+		if allowed(board, r, 1) {
+
+			placethirdqueen(board)
+
+		} else {
+			board[r][1] = 0
+		}
+	}
+
 }
 
 //Givefourqueensposition gives position of each of the four queens
@@ -70,11 +139,11 @@ func Givefourqueensposition() {
 	var board = [4][4]int{{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
 
 	for r := 0; r < N; r++ {
-		fmt.Println("----")
+		fmt.Println("--------")
+
+		//place q on col 1
 		board[r][0] = 1
-		for r2 := 0; r2 < N; r2++ {
-			fn0(board, r2, r)
-		}
+		placesecondqueen(board)
 		board[r][0] = 0
 	}
 
